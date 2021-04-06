5794b7f29c2005ea4877af8e9494e4852b58f2ad
sonchaegeon <thscorjs1002@naver.com>
2021-04-06 20:22:50 +0900

    [UPDATE] functions part two

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -5,21 +5,14 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("hello go!")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	totalLength, upperName := lenAndUpper("sonchaegeon")
-	fmt.Println(multiply(2, 4))
-	fmt.Println(totalLength, upperName)
-	repeatMe("son", "go", "park", "lee", "kim")
+	totalLength, up := lenAndUpper("sonchaegeon")
+	fmt.Println(totalLength, up)
 }
