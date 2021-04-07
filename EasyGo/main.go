8ca70a4ceed07ad08de6d1ec957630ca54977aa6
sonchaegeon <thscorjs1002@naver.com>
2021-04-07 09:05:32 +0900

    [UPDATE] for, range, ...args

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -1,18 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("hello go!")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLength, up := lenAndUpper("sonchaegeon")
-	fmt.Println(totalLength, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
