0128e256d0e2c09c2f80c39fd84388be73b70189
sonchaegeon <thscorjs1002@naver.com>
2021-04-11 14:35:31 +0900

    [UPDATE] structs

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
-func main() {
-	scg := map[string]string{"name": "sonchaegeon", "age": "18"}
-	fmt.Println("name: " + scg["name"] + " age: " + scg["age"])
+type person struct {
+	name string
+	age int
+	favFood []string
+}
 
-	for _, value := range scg {
-		fmt.Println(value)
-	}
+func main() {
+	favFood := []string{"pizza", "hamburger"}
+	scg := person{"sonchaegeon", 18, favFood}
+	fmt.Println(scg)
+	fmt.Println(scg.favFood)
 }
