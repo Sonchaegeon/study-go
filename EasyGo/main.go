6d98d7104e80ed9d5e9b335d36901f17386fa984
sonchaegeon <thscorjs1002@naver.com>
2021-04-11 15:37:43 +0900

    [UPDATE] search dictionary

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	"study-go/EasyGo/accounts"
+	"study-go/EasyGo/myDict"
 )
 
 func main() {
-	account := accounts.NewAccount("sonchaegeon")
-	account.Deposit(200000)
-	fmt.Println(account)
+	dictionary := myDict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(definition)
 }
