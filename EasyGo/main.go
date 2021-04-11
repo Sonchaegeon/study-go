f6542469dedbd61592a8f1a3c84bddee0b613738
sonchaegeon <thscorjs1002@naver.com>
2021-04-11 15:47:52 +0900

    [UPDATE] Add func

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	dictionary := myDict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	err := dictionary.Add("hello", "Greeting")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(definition)
+	definition, _ := dictionary.Search("hello")
+	fmt.Println("found: hello", "definition:", definition)
+	err2 := dictionary.Add("hello", "Greeting")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
--- EasyGo/myDict/myDict.go
+++ EasyGo/myDict/myDict.go
@@ -6,6 +6,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("not found")
+var errWordExists = errors.New("that word already exist")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +15,17 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+
+	return nil
 }
