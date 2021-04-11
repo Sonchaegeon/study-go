1505f85440fcf34043db92cc3b4b8601fdadc997
sonchaegeon <thscorjs1002@naver.com>
2021-04-11 15:26:22 +0900

    [UPDATE] ChangeOwner, Owner, String func

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"study-go/EasyGo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("sonchaegeon")
 	account.Deposit(200000)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(300000)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
