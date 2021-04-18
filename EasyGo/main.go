f7037bdb15d96bb82e7bac9a9813c258e03aa715
sonchaegeon <thscorjs1002@naver.com>
2021-04-18 16:53:42 +0900

    [UPDATE] Channels Recap

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -6,17 +6,18 @@ import (
 )
 
 func main() {
-	c := make(chan bool)
-	people := [2]string{"son", "chaegeon"}
+	c := make(chan string)
+	people := [5]string{"son", "chae", "geon", "go", "backend"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	fmt.Println("Waiting for messages...")
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is sexy"
 }
