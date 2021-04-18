4bb4f66217b53ceaed12dd73db58fb33a6d0f0ee
sonchaegeon <thscorjs1002@naver.com>
2021-04-18 15:47:03 +0900

    [UPDATE] Goroutines

--- EasyGo/main.go
+++ EasyGo/main.go
@@ -1,42 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go sexyCount("son")
+	go sexyCount("chaegeon")
+	time.Sleep(time.Second * 5)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	res, err := http.Get(url)
-	if err != nil || res.StatusCode >= 400 {
-		fmt.Println(err, res.StatusCode)
-		return errRequestFailed
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
